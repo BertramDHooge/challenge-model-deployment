bb6d2def0c53b602cb2dbbaf811b325ce153c330
BertramDHooge <bertramdhooge@gmail.com>
2022-05-06 12:26:35 +0200

    ehh

--- app.py
+++ app.py
@@ -17,6 +17,98 @@ def home():
         return render_template("documentation.html", title="Home", cities=list(cities_df.city))
 
     if request.method == "POST":
+        #try except => return expected format at fail give details
+        predictable_data = {"Send requests to /predict following this format": {
+            "Living area": int,
+            "Land area": int,
+            "City": "String containing a belgian city",
+            "Property": ["APARTMENT_BLOCK",
+                         "BUNGALOW",
+                         "CASTLE",
+                         "CHALET",
+                         "COUNTRY_COTTAGE",
+                         "EXCEPTIONAL_PROPERTY",
+                         "FARMHOUSE",
+                         "HOUSE",
+                         "MANOR_HOUSE",
+                         "MANSION",
+                         "MIXED_USE_BUILDING",
+                         "OTHER_PROPERTY",
+                         "TOWN_HOUSE",
+                         "VILLA"],
+            "Bedrooms": int,
+            "Garden": bool,
+            "Kitchen": bool,
+            "Pool": bool,
+            "Furnished": bool,
+            "Fireplace": bool,
+            "Terrace": bool,
+            "Condition": bool
+            }
+        }
+
+        return predictable_data
+
+
+@app.route("/web", methods=["GET", "POST"])
+def web():
+    if request.method == "GET":
+        return render_template("index.html", cities=cities_df.city)
+
+    if request.method == "POST":
+        data = request.form
+        location = data.get("Location")
+        property_type = data.get("Property")
+        condition = data.get("Condition")
+        kitchen = data.get("Kitchen")
+        furnished = data.get("Furnished")
+        fireplace = data.get("Fireplace")
+        terrace = data.get("Terrace")
+        garden = data.get("Garden")
+        pool = data.get("Pool")
+        land_area = data.get("landArea")
+        living_area = data.get("livingArea")
+        bedrooms = data.get("Bedrooms")
+
+        predictable_data = {'Number of bedrooms': bedrooms,
+                            'Living area': living_area,
+                            'Kitchen': kitchen is not None,
+                            'Furnished': furnished is not None,
+                            'Open fireplace': fireplace is not None,
+                            'Terrace': terrace is not None,
+                            'Garden': garden is not None,
+                            'Surface area land': land_area,
+                            'Pool': pool is not None,
+                            'Condition': condition is not None,
+                            'longitude': cities_df[cities_df["city"] == location].lng,
+                            'latitude': cities_df[cities_df["city"] == location].lat,
+                            'property_subtype_APARTMENT_BLOCK': 0,
+                            'property_subtype_BUNGALOW': 0,
+                            'property_subtype_CASTLE': 0,
+                            'property_subtype_CHALET': 0,
+                            'property_subtype_COUNTRY_COTTAGE': 0,
+                            'property_subtype_EXCEPTIONAL_PROPERTY': 0,
+                            'property_subtype_FARMHOUSE': 0,
+                            'property_subtype_HOUSE': 0,
+                            'property_subtype_MANOR_HOUSE': 0,
+                            'property_subtype_MANSION': 0,
+                            'property_subtype_MIXED_USE_BUILDING': 0,
+                            'property_subtype_OTHER_PROPERTY': 0,
+                            'property_subtype_TOWN_HOUSE': 0,
+                            'property_subtype_VILLA': 0
+                           }
+
+        predictable_data[property_type] = 1
+        return render_template("predict.html", prediction=model.predict(predictable_data))
+
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+
+    if request.method == "GET":
+        return render_template("predict_help.html")
+
+    if request.method == "POST":
         #try except => return expected format at fail give details 
         data = json.loads(request.data)
         predictable_data = {'Number of bedrooms': data["data"]["Bedrooms"],
@@ -29,54 +121,29 @@ def home():
                             'Surface area land': data["data"]["Land area"],
                             'Pool': data["data"]["Pool"],
                             'Condition': data["data"]["Condition"],
-                            'longitude': cities_df[cities_df["city"] == data["data"]["City"]].longitude,
-                            'latitude': cities_df[cities_df["city"] == data["data"]["City"]].latitude,
-                            'Property subtype_APARTMENT_BLOCK': 0,
-                            'Property subtype_BUNGALOW': 0,
-                            'Property subtype_CASTLE': 0,
-                            'Property subtype_CHALET': 0,
-                            'Property subtype_COUNTRY_COTTAGE': 0,
-                            'Property subtype_EXCEPTIONAL_PROPERTY': 0,
-                            'Property subtype_FARMHOUSE': 0,
-                            'Property subtype_HOUSE': 0,
-                            'Property subtype_MANOR_HOUSE': 0,
-                            'Property subtype_MANSION': 0,
-                            'Property subtype_MIXED_USE_BUILDING': 0,
-                            'Property subtype_OTHER_PROPERTY': 0,
-                            'Property subtype_TOWN_HOUSE': 0,
-                            'Property subtype_VILLA:': 0
+                            'longitude': cities_df[cities_df["city"] == data["data"]["City"]].lng,
+                            'latitude': cities_df[cities_df["city"] == data["data"]["City"]].lat,
+                            'property_subtype_APARTMENT_BLOCK': 0,
+                            'property_subtype_BUNGALOW': 0,
+                            'property_subtype_CASTLE': 0,
+                            'property_subtype_CHALET': 0,
+                            'property_subtype_COUNTRY_COTTAGE': 0,
+                            'property_subtype_EXCEPTIONAL_PROPERTY': 0,
+                            'property_subtype_FARMHOUSE': 0,
+                            'property_subtype_HOUSE': 0,
+                            'property_subtype_MANOR_HOUSE': 0,
+                            'property_subtype_MANSION': 0,
+                            'property_subtype_MIXED_USE_BUILDING': 0,
+                            'property_subtype_OTHER_PROPERTY': 0,
+                            'property_subtype_TOWN_HOUSE': 0,
+                            'property_subtype_VILLA': 0
                            }
 
-        predictable_data[f"Property subtype{data['data']['Property'].upper()}"] = 1
+        predictable_data[f"property_subtype_{data['data']['Property'].upper()}"] = 1
 
         df = pd.DataFrame([predictable_data])
-        df.drop("Property", axis=1, inplace=True)
-        df[f"Property_{data['data']['Property'].upper()}"] = [1]
-        return data
-
-        if len(data) != 3:
-            return {
-                "error": "3 fields expected (salary, bonus, taxes)",
-                "request": data
-            }
-
-        try:
-            return {
-                "result": int(data["salary"]) +
-                int(data["bonus"]) -
-                
-                int(data["taxes"])
-            }
-        except:
-            return {
-                "error": "expected numbers, got strings",
-                "request": data
-            }
-
-@app.route("/web", methods=["GET"])
-def web():
-    return render_template("index.html", cities=cities_df.city)
-
+        prediction = model.predict(df)[0]
+        return render_template("predict.html", prediction=prediction)
 
 if __name__ == "__main__":
     app.run()
